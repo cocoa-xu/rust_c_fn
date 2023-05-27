6d7e221e2c405390a1931e34f017192e858c8167
Cocoa <i@uwucocoa.moe>
2023-05-27 12:05:51 +0100

    updated rust example

--- lib/rust_nif.ex
+++ lib/rust_nif.ex
@@ -1,7 +1,6 @@
 defmodule RustNIF do
   use Rustler, otp_app: :rust_c_fn, crate: :rustnif
 
-  def pointer_to_my_add_rs(), do: error()
-
-  defp error, do: :erlang.nif_error(:nif_not_loaded)
+  def rust_adbc_statement_execute_query(_func, _stmt, _error, _ref),
+    do: :erlang.nif_error(:nif_not_loaded)
 end
--- native/rustnif/src/lib.rs
+++ native/rustnif/src/lib.rs
@@ -1,14 +1,49 @@
-extern "C" fn my_add_rs(a: i64, b: i64) -> i64 {
-    a + b
+use rustler::{Env, Term, Error, ResourceArc};
+use std::sync::Mutex;
+
+#[repr(C)]
+struct ArrowArrayStream {
+    get_schema: usize,
+    get_next: usize,
+    get_last_error: usize,
+    release: usize,
+    private_data: usize
+}
+
+struct ArrowArrayStreamResource {
+    pub stream: Mutex<ArrowArrayStream>,
+}
+
+fn load(env: Env, _: Term) -> bool {
+    rustler::resource!(ArrowArrayStreamResource, env);
+    true
 }
 
 #[rustler::nif]
-fn pointer_to_my_add_rs() -> u64 {
-    unsafe {
-        std::mem::transmute::<_, u64>(my_add_rs as extern "C" fn(_, _) -> _)
+fn rust_adbc_statement_execute_query(func_ptr: u64, stmt_ptr: u64, error_ptr: u64, _ref: Term) -> Result<(ResourceArc<ArrowArrayStreamResource>, i64), Error> {
+    let ptr = func_ptr as *const ();
+    let code: extern "C" fn(u64, &mut ArrowArrayStream, &mut i64, u64) -> u8 = unsafe { std::mem::transmute(ptr) };
+    let mut rows_affected: i64 = 0;
+    let mut stream = ArrowArrayStream {
+        get_schema: 0, 
+        get_next: 0, 
+        get_last_error: 0, 
+        release: 0, 
+        private_data: 0
+    };
+    let ok = (code)(stmt_ptr, &mut stream, &mut rows_affected, error_ptr);
+
+    if ok != 0 {
+        Err(Error::Term(Box::new(error_ptr)))
+    } else {
+        let resource = ResourceArc::new(ArrowArrayStreamResource {
+            stream: Mutex::new(stream),
+        });
+
+        Ok((resource, rows_affected))
     }
 }
 
 rustler::init!("Elixir.RustNIF", [
-    pointer_to_my_add_rs
-]);
+    rust_adbc_statement_execute_query
+], load = load);
